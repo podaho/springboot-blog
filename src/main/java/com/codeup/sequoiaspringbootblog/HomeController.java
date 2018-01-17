ce15ba7738407d1b08cf7d7aff3b5fa2f1a12f09
Po <po.grammer.lin@gmail.com>
2018-01-17 17:03:40 -0600

    Finished Dice Exercise

--- src/main/java/com/codeup/sequoiaspringbootblog/HomeController.java
+++ src/main/java/com/codeup/sequoiaspringbootblog/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
-    public String landingString() {
-        return "This is the landing page!";
+    public String index() {
+        return "home";
     }
 }
