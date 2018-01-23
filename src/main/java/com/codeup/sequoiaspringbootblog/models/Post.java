8112ffd4b547519a7ae1276be9a4b81758f8d13a
Po <po.grammer.lin@gmail.com>
2018-01-23 13:35:07 -0600

    Relationships Exercise In Progress

--- src/main/java/com/codeup/sequoiaspringbootblog/models/Post.java
+++ src/main/java/com/codeup/sequoiaspringbootblog/models/Post.java
@@ -14,6 +14,10 @@ public class Post {
     @Column(columnDefinition = "TEXT", nullable = false)
     private String body;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
---
+++ src/main/java/com/codeup/sequoiaspringbootblog/models/User.java
@@ -0,0 +1,62 @@
+package com.codeup.sequoiaspringbootblog.models;
+
+import javax.persistence.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(cascade = )
+
+    public User(long id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
